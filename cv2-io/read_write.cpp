70bcb8c5d97fdabd4eca33e1d7d92da34769ccda
Madhav Humagain <scimad2.71@gmail.com>
2019-12-12 12:48:31 +0545

    Include and compile success of opencv2

---
+++ cv2-io/read_write.cpp
@@ -0,0 +1,8 @@
+#include <opencv2/core.hpp>
+#include <iostream>
+
+int main(){
+    std::cout <<".. and miles to go before I sleep." << std::endl;
+    std::cout <<".................................." << std::endl;
+    return 0;
+}
