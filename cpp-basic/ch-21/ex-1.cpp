589aa7b9a4fd1bef649b48332b34f08344efc188
scimad <scimad2.71@gmail.com>
2020-05-09 13:21:10 +0545

    Add CMakeLists | Add demo in ex-1

--- cpp-basic/ch-21/ex-1.cpp
+++ cpp-basic/ch-21/ex-1.cpp
@@ -13,5 +13,15 @@ int main(int argc, char const *argv[])
 
     *d_ptr = 1.51;
     std::cout<<d<<std::endl;
+
+    double x;
+
+    x = *d_ptr;
+    std::cout<<x<<std::endl;
+
+    *d_ptr = 11.22;
+    std::cout<<x<<std::endl;
+    std::cout<<d<<std::endl;
+
     return 0;
 }
