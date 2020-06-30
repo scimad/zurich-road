588fe5ea7ce118e6055af4a66ab4f878d34cf5e0
scimad <scimad2.71@gmail.com>
2020-07-01 01:45:03 +0545

    Initial commit for Bjarne 4th Edition

---
+++ cpp-basic/bjarne/ch-3/initializing_containers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main(int argc, char const *argv[])
+{
+    class Vector
+    {
+    public:
+        Vector(std::initializer_list<double>);
+        ~Vector();
+        
+    };
+
+    return 0;
+}
