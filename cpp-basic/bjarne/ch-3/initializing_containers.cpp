963c8568e0edc70453873f69c81f75773e1dbce3
scimad <scimad2.71@gmail.com>
2020-07-01 11:30:25 +0545

    Add initializing containers

--- cpp-basic/bjarne/ch-3/initializing_containers.cpp
+++ cpp-basic/bjarne/ch-3/initializing_containers.cpp
@@ -1,13 +1,54 @@
 #include <iostream>
-int main(int argc, char const *argv[])
-{
-    class Vector
+#include <fstream>
+
+class Vector
     {
+    private:
+        double* elem;
+        int sz;
     public:
-        Vector(std::initializer_list<double>);
-        ~Vector();
+        // Vector (){ Vector(0); } //This gives seg. fault but the following doesn't
+        Vector ():elem{new double[0]},sz{0}{} //This works fine but above doesn't
+
+
+        Vector (int s):elem{new double[s]},sz{s}
+        {
+            for (int i=0; i!=s; ++i)
+            { 
+                elem[i] = 0;
+            }
+        }
+        Vector(std::initializer_list<double> l):Vector(0)
+        {
+            for (auto a:l){
+                this->push_back(a);
+            }
+        };
+        ~Vector(){delete[] elem;};
+        double& operator[](int i);
+        int size() const;
+        void push_back(double d)
+        {   elem[sz++] = d;
+
+        };
+
+        void show()
+        {
+            for (int i=0; i!=sz; i++){
+                std::cout<<elem[i]<<std::endl;
+            }
+            if (sz == 0)
+                std::cout<<"Nothing to show"<<std::endl;
         
+        };
     };
 
-    return 0;
+int main(int argc, char const *argv[])
+{
+    Vector v1 = {1, 11, 111};
+    v1.show();
+
+    Vector v2;
+    v2.show();
+
 }
