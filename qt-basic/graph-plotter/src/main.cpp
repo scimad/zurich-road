4e5253c13670c7efed6e792f89621bce3d8af1ee
scimad <scimad2.71@gmail.com>
2020-08-02 22:03:56 +0545

    Add basic Qt example for drawing curve

---
+++ qt-basic/graph-plotter/src/main.cpp
@@ -0,0 +1,24 @@
+#include <QtGui/QtGui>
+#include <QtQuick/QtQuick>
+
+#include "beziercurve.hpp"
+
+//! [1]
+
+
+int main(int argc, char **argv)
+{
+    QGuiApplication app(argc, argv);
+
+    qmlRegisterType<BezierCurve>("CustomGeometry", 1, 0, "BezierCurve");
+
+    QQuickView view;
+    QSurfaceFormat format = view.format();
+    format.setSamples(16);
+    view.setFormat(format);
+    view.setSource(QUrl(QStringLiteral("qrc:/src/main.qml")));
+    view.show();
+
+    app.exec();
+}
+//! [1]
